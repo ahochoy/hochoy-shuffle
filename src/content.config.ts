a75a6f49cf13213d978fa2f8d82d3447cc864fc2
Andrew HoChoy <a.hochoy@gmail.com>
2025-03-19 17:03:40 -0700

    Defining content structure

---
+++ src/content.config.ts
@@ -0,0 +1,35 @@
+// 1. Import utilities from `astro:content`
+import { defineCollection, z } from 'astro:content';
+
+// 2. Import loader(s)
+import { glob, file } from 'astro/loaders';
+
+// 3. Define your collection(s)
+const projects = defineCollection({
+    loader: glob({ pattern: "**/*.md", base: "./src/data/projects" }),
+    schema: {
+        title: z.string(),
+        description: z.string(),
+        imageUrl: z.string(),
+        link: z.string(),
+        tags: z.array(z.string()),
+    }
+});
+
+const experienceSchema = {
+    title: z.string(),
+    description: z.string(),
+};
+
+const currentExperiences = defineCollection({
+    loader: file("src/data/experiences.json", { parser: (text) => JSON.parse(text).current }),
+    schema: experienceSchema
+});
+
+const pastExperiences = defineCollection({
+    loader: file("src/data/experiences.json", { parser: (text) => JSON.parse(text).past }),
+    schema: experienceSchema
+});
+
+// 4. Export a single `collections` object to register your collection(s)
+export const collections = { projects, currentExperiences, pastExperiences };
---
+++ src/data/projects/lit-food-tours.md
@@ -0,0 +1,6 @@
+---
+title: "Lit Food Tours"
+link: "url-incoming.com"
+tags: ["AI / Groq API", "Python", "Azure", "Docker", "Github Actions"]
+---
+Let AI plan a food tour for you and one of your favorite characters and on an imaginative culinary adventure! What would Southern favorites would Sherlock Holmes enjoy? What about Hello Kitty on a Mexican vacation? Imagine Thor eating Thai?
