6e36e02f558ee9a78543bd9eb6dc388e912889ca
Andrew HoChoy <a.hochoy@gmail.com>
2025-03-25 22:13:31 -0700

    Updating footer and header components

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -1,7 +1,13 @@
-import Spline from '@splinetool/react-spline';
+import React, { Suspense } from 'react';
+import placeholderImage from '../assets/welcome-placeholder.png'
 
+const Spline = React.lazy(() => import('@splinetool/react-spline'));
+
+{/* <img src={placeholderImage.src} /> */}
 export default function Scene() {
   return (
-    <Spline scene="https://prod.spline.design/EgnnVtmMIwfPumjP/scene.splinecode" />
+    <Suspense fallback={<div>LOADING...</div>}>
+      <Spline scene="https://prod.spline.design/EgnnVtmMIwfPumjP/scene.splinecode" loading="eager" />
+    </Suspense>
   );
 }
